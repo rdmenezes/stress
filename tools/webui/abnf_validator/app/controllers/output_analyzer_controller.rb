1fa3e846c58c3f37446af480991f0a98839ad623
matteo.rosi <matteo.rosi@b1e273ac-09d0-c771-20cd-da71f0ac3416>
2011-09-05 15:11:37 +0000

    add plugin output analyzer

---
+++ tools/webui/abnf_validator/app/controllers/output_analyzer_controller.rb
@@ -0,0 +1,53 @@
+class OutputAnalyzerController < ApplicationController
+	require 'lib/output_analyzer.rb'
+	
+	#GET NEEDED INFORMATION FOR PLUGIN SYSTEM AND LIBRARIES
+	def index
+		@simulations = Simulation.find_all_by_username(session[:username])
+		@plugins = Array.new
+		xml_file_list = Dir.glob("app/*.xml")
+		xml_file_list.each do |f|
+			@plugins << Plugin.new(f)
+		end
+		@libraries = Library.find(:all)
+		respond_to {|format| format.html}
+	end
+	
+	def select_simulation
+		@files = Dir.glob("results/#{params[:output]}/*")
+		@files.sort!{|x,y| x.split("/").last <=> y.split("/").last }
+		@simulation_selected=params[:output]
+		sleep 3
+		render :partial => "output_body"
+		#render :update do |page|
+         # page.replace_html "right_column", :partial => 'output_body'
+        # end
+	end
+	
+	def get_l_distance
+		@simulation_selected=params[:output]
+		render :text => '<div style="clear:both;"></div> Calcolo della distanza di Levenshtein '+@simulation_selected.to_s
+	end
+	
+	def show_loading
+		render :partial => "loading"
+	end
+	
+	#SAVES AN ABNF FILE FROM FORM INFORMATION
+#	def save_abnf_file
+#		begin
+#			file = AbnfFile.create_new_file(file_name, params[:library_id], session[:user_id])
+#			abnf_content = "First arg: #{params[:first_arg]}, second_arg: #{params[:second_arg]}"
+#			file.update_attributes(:content => abnf_content)
+#			session[:messages] = {:type => "ok", :msg => "File Saved!"}
+#		rescue Exceptions::MissingParameters
+#			session[:messages] = {:type => "err", :msg => "Missing file name!"}
+#		rescue Exceptions::InvalidLibraryId
+#			session[:messages] = {:type => "err", :msg => "Invalid Library id!"}			
+#		rescue
+#			session[:messages] = {:type => "err", :msg => "Generic error, please contact administrator"}
+#		end
+#		render :text => "true"
+#	end
+	
+end
