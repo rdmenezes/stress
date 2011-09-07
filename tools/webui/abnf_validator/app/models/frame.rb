07c3d4afefbac2d24a573279b73c33b5bf607c00
matteo.rosi <matteo.rosi@b1e273ac-09d0-c771-20cd-da71f0ac3416>
2011-09-07 13:55:16 +0000

    add parsing xml file

--- tools/webui/abnf_validator/app/models/frame.rb
+++ tools/webui/abnf_validator/app/models/frame.rb
@@ -1,8 +1,23 @@
 class Frame < ActiveRecord::Base
 	belongs_to :testcase
-	acts_as_list :scope => :testcase
+	#acts_as_list :scope => :testcase
+	
+	def printHTML
+		out = self.position.to_s+": "
+		out+= self.type
+		
+		return out
+	end
 end
+
 class ReadFrame < Frame
+
 end
+
 class SentFrame < Frame
+
+end
+
+class NoResponse < ReadFrame
+
 end
--- tools/webui/abnf_validator/app/models/testcase.rb
+++ tools/webui/abnf_validator/app/models/testcase.rb
@@ -2,4 +2,10 @@ class Testcase < ActiveRecord::Base
 	belongs_to :simulation
 	has_many :frames, :order => :position
 	has_many :monitorreports
+	
+	def printHTML
+		out = "ID: "+self.id_anomaly.to_s
+		out += " RUN: "+self.run.to_s
+		return out
+	end
 end
